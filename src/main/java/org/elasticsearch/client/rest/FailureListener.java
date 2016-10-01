549c938405eed80444b65c6968ff5a7abfa291a9
Brandon Kearby <brandon.kearby+github@gmail.com>
2016-10-01 12:55:03 -0500

    Adding Builder for RestClient

---
+++ src/main/java/org/elasticsearch/client/rest/FailureListener.java
@@ -0,0 +1,8 @@
+package org.elasticsearch.client.rest;
+
+/**
+ * @author Brandon Kearby
+ *         October 01, 2016
+ */
+public class FailureListener {
+}
---
+++ src/main/java/org/elasticsearch/client/rest/HttpClientConfigCallback.java
@@ -0,0 +1,8 @@
+package org.elasticsearch.client.rest.support;
+
+/**
+ * @author Brandon Kearby
+ *         October 01, 2016
+ */
+public class HttpClientConfigCallback {
+}
